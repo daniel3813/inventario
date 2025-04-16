2754caa7102e49c17e211d445f06696c325ce8e0
Daniel3813 <danielcastellanos3813@gmail.com>
2025-04-16 15:22:16 -0500

    JavaScript - CRUD (READ) - HTTP_GET - Async Await - Fetch API

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
 import express from "express";
 import categoriaRouter from "./routes/categorias.routes.js"
+import cors from "cors"
 
 const app=express();
 
 app.set("port",5000)
+app.use(cors());
+
 
 app.use("/api/categorias",categoriaRouter);
+app.use(express.json());
 
 export default app;
 
